6df4a7ecd398c5543b2ad633aa61f37edec3757b
raska26 <radityasandika51@gmail.com>
2026-01-16 10:16:22 +0700

    Menambahkan ikon Ionicons dan Image pada tampilan

--- mobile/app/(auth)/sign-in.jsx
+++ mobile/app/(auth)/sign-in.jsx
@@ -1,2 +1,8 @@
 import { useSignIn } from "@clerk/clerk-expo";
 import { useRouter } from "expo-router";
+import {
+  View
+} from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+
+import { Image } from "expo-image";
