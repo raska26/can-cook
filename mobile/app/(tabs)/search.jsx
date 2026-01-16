4845e94de6fec30f134e56bf69041d0ccade12bd
rofiivor <masterrofi53995@gmail.com>
2026-01-16 14:50:06 +0700

    tambah dependensi pencarian, gaya, dan komponen UI

--- mobile/app/(tabs)/search.jsx
+++ mobile/app/(tabs)/search.jsx
@@ -6,3 +6,10 @@ import {
   TouchableOpacity,
   FlatList,
 } from "react-native";
+import { MealAPI } from "../../services/mealAPI";
+import { useDebounce } from "../../hooks/useDebounce";
+import { searchStyles } from "../../assets/styles/search.styles";
+import { COLORS } from "../../constants/colors";
+import { Ionicons } from "@expo/vector-icons";
+import RecipeCard from "../../components/RecipeCard";
+import LoadingSpinner from "../../components/LoadingSpinner";
