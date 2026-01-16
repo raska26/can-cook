100f674a627780911d15bff4c743915d0f709a43
raska26 <radityasandika51@gmail.com>
2026-01-16 10:43:11 +0700

    tambah import

--- mobile/app/(auth)/sign-in.jsx
+++ mobile/app/(auth)/sign-in.jsx
@@ -1,12 +1,19 @@
 import { useSignIn } from "@clerk/clerk-expo";
 import { useRouter } from "expo-router";
 import {
+  View,
+  Text,
+  Alert,
+  KeyboardAvoidingView,
+  Platform,
   ScrollView,
-  View
+  TextInput,
+  TouchableOpacity,
 } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
-
 import { Image } from "expo-image";
+import { authStyles } from "../../assets/styles/auth.styles";
+import { COLORS } from "../../constants/colors";
 
 const SignInScreen = () => {
   const router = useRouter();
