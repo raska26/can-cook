252f2eb0279804ccad6c10be15a9b78cc97504b5
raska26 <radityasandika51@gmail.com>
2026-01-16 18:17:53 +0700

    nambah loading state

--- mobile/app/(auth)/_layout.jsx
+++ mobile/app/(auth)/_layout.jsx
@@ -1,10 +1,18 @@
 import { Redirect, Stack } from "expo-router";
 import { useAuth } from "@clerk/clerk-expo";
+import { ActivityIndicator, View } from "react-native";
 
 export default function AuthRoutesLayout() {
-  const { isSignedIn } = useAuth();
+  const { isSignedIn, isLoaded } = useAuth();
 
-  if (isSignedIn) return <Redirect href={"/"} />;
 
-  return <Stack screenOptions={{ headerShown: false }} />;
+  if (!isLoaded) {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <ActivityIndicator size="large" />
+      </View>
+    );
+  }
+
+
 }
