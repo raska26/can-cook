d64114f063359b91192db3cf5d676ac60a039c8a
raska26 <radityasandika51@gmail.com>
2026-01-17 09:01:29 +0700

    benerin error

--- mobile/app/_layout.jsx
+++ mobile/app/_layout.jsx
@@ -1,10 +1,14 @@
-import { ClerkProvider } from '@clerk/clerk-expo';
-import { Slot } from 'expo-router';
+import { Slot } from "expo-router";
+import { ClerkProvider } from "@clerk/clerk-expo";
+import { tokenCache } from "@clerk/clerk-expo/token-cache";
+import SafeScreen from "@/components/SafeScreen";
 
 export default function RootLayout() {
   return (
-    <ClerkProvider publishableKey={process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY}>
-      <Slot />
+    <ClerkProvider tokenCache={tokenCache}>
+      <SafeScreen>
+        <Slot />
+      </SafeScreen>
     </ClerkProvider>
   );
 }
--- mobile/constants/colors.js
+++ mobile/constants/colors.js
@@ -97,5 +97,5 @@ export const THEMES = {
   roseGold: roseGoldTheme,
 };
 
-// 👇 change this to switch theme
+
 export const COLORS = THEMES.purple;
